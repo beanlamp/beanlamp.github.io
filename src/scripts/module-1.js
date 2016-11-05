7b9fcd2b8b9ab7919c6cb12d4065206a374700ea
Christopher Fujino <christopherfujino@gmail.com>
2016-11-04 17:50:36 -1000

    start beanlamp skeleton

--- src/scripts/module-1.js
+++ src/scripts/module-1.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
 
 export const query = () => {
-  $('#target-well').text('This text injected from JavaScript')
+//  $('#target-well').text('This text injected from JavaScript')
 }
